2f3a4b76aec434850278dc79ca305c95e1fa9718
Abisaac1809 <abisaaccarmona@gmail.com>
2025-08-21 06:36:25 -0400

    feat:vistas para ver cruceros disponible, para editar cada crucero y para registrar uno nuevo

--- Administrador_Cruceros/settings.py
+++ Administrador_Cruceros/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "myApp"
 ]
 
 MIDDLEWARE = [
@@ -111,10 +112,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = 'Static/'
 
-STATIC_URL = 'static/'
+# Media files (user-uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- myApp/admin.py
+++ myApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Crucero, TipoHabitacion, Habitacion, Instalacion
 
-# Register your models here.
+admin.site.register(Crucero)
+admin.site.register(TipoHabitacion)
+admin.site.register(Habitacion)
+admin.site.register(Instalacion)
